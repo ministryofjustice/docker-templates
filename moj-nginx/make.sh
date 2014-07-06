34bcd5e364a8f692cfe1117a0a4321b5328926e1
paulrhodes <paul.rhodes@digital.justice.gov.uk>
2014-07-06 21:26:46 +0100

    Initial Commit

---
+++ moj-nginx/make.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+DOCKER_NOPUSH=true
+DOCKER_NORMI=true
+
+DEFAULT_DOCKERREPO="docker.local:5000"
+DEFAULT_DOCKERTAG="moj-base-nginx"
+
+DOCKERFILE="docker/rails/Dockerfile"
+DOCKERREPO="${DOCKERREPO:-$DEFAULT_DOCKERREPO}"
+DOCKERTAG="${DOCKERTAG:-$DEFAULT_DOCKERTAG}"
+
+if [ -n "$1" ]; then
+  TAG="${DOCKERREPO}/${DOCKERTAG}:$1"
+else
+  TAG="${DOCKERREPO}/${DOCKERTAG}"
+fi
+
+docker build -t ${TAG} --force-rm=true .
+
+if [ -z "$DOCKER_NOPUSH" ]; then
+  echo "+ docker push ${TAG}"
+  docker push ${TAG}
+fi
+
+if [ -z "$DOCKER_NORMI" ]; then
+  echo "+ docker rmi ${TAG}"
+  docker rmi ${TAG}
+fi
