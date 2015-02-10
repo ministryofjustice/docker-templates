0a663e0cf138eea1aabc7c3db7c96906bf08b2e7
mattmb <matthew.mead-briggs@digital.justice.gov.uk>
2015-02-10 17:46:00 +0000

    Add tagging based on the git tag to the docker image. This way when you build images they will get an extra tag based on the current git tag (if you are on a tag).

--- make.sh
+++ make.sh
@@ -1,8 +1,10 @@
-
 CWD=`pwd`
-
+#This gets the tag that's currently checked out
+TAG=`git name-rev --tags --name-only $(git rev-parse HEAD)|cut -f1 -d '^'`
 for img in moj-base moj-nginx moj-ruby moj-peoplefinder light; do
-	cd $CWD/$img
-	docker build -t $img .
+    cd $CWD/$img
+    docker build -t $img .
+    if [ "$TAG" != "undefined" ]; then
+        docker tag $img:latest $img:$TAG
+    fi
 done
-
