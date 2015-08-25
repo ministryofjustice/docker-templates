6d0c25f89144a35a67766a0e2c1d01ce00704b7f
Jakub Novotny <novotny.jakub@gmail.com>
2015-08-25 17:28:09 +0100

    Change make.sh accordingly to the current available images

--- make.sh
+++ make.sh
@@ -1,7 +1,7 @@
 
 CWD=`pwd`
 
-for img in moj-base moj-nginx moj-ruby moj-peoplefinder light; do
+for img in moj-base moj-nginx light ruby-app-base ruby-webapp-onbuild; do
 	cd $CWD/$img
 	docker build -t $img .
 done
